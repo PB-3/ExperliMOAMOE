5adfdbe77eb2c1749b6a6888e8bc2ce17fe47d8f
Teray Raj <116900119+PB-3@users.noreply.github.com>
2023-06-21 15:49:45 +0200

    Add files via upload

---
+++ Choix_Jeu.js
@@ -0,0 +1,72 @@
+
+function choix_scenario(event) {
+  event.preventDefault();
+  var form = event.target;
+  var selectedChoice = null;
+  
+  // Parcours des éléments du formulaire pour trouver le choix sélectionné
+  for (var i = 0; i < form.elements.length; i++) {
+    var element = form.elements[i];
+    if (element.type === "radio" && element.checked) {
+      selectedChoice = element.value;
+      break;
+    }
+  }
+  
+  if (selectedChoice === 'MOA') {
+    window.location.href='MOA/Jeu_MOA.php';
+    console.log("Choix : MOA");
+  } else if (selectedChoice === 'MOE') {
+
+     // Obtenir l'URL courante
+    var currentUrl = window.location.href;
+
+    // Supprimer le nom du fichier (fichier.html) de l'URL
+    var parentUrl = currentUrl.substring(0, currentUrl.lastIndexOf("/"));
+
+    // Remonter au dossier parent (last)
+    var grandParentUrl = parentUrl.substring(0, parentUrl.lastIndexOf("/"));
+
+    // Afficher le chemin d'accès au dossier parent (last)
+    console.log(grandParentUrl);
+    window.location.href=grandParentUrl+'/MOE-code/Jeu_MOE.php';
+    
+  
+  } else if (selectedChoice === 'PRINCE2') {
+      // Obtenir l'URL courante
+    var currentUrl = window.location.href;
+
+    // Supprimer le nom du fichier (fichier.html) de l'URL
+    var parentUrl = currentUrl.substring(0, currentUrl.lastIndexOf("/"));
+
+  // Remonter au dossier parent (last)
+    var grandParentUrl = parentUrl.substring(0, parentUrl.lastIndexOf("/"));
+
+  // Afficher le chemin d'accès au dossier parent (last)
+    console.log(grandParentUrl);  
+    window.location.href=grandParentUrl+'/prince2_pmbok/Jeu_PRINCE2_PMBOK.php';
+  }
+  else if (selectedChoice === 'NORME'){
+     // Obtenir l'URL courante
+    var currentUrl = window.location.href;
+
+    // Supprimer le nom du fichier (fichier.html) de l'URL
+    var parentUrl = currentUrl.substring(0, currentUrl.lastIndexOf("/"));
+
+    // Remonter au dossier parent (last)
+    var grandParentUrl = parentUrl.substring(0, parentUrl.lastIndexOf("/"));
+
+    // Afficher le chemin d'accès au dossier parent (last)
+    console.log(grandParentUrl);
+    var progress = document.querySelector('.progress').style.display = 'hidden';
+    var bar = document.querySelector('.progress-bar').style.display='hidden';
+    window.location.href=grandParentUrl+'/norme_qualite/norme_qualite.php';
+
+
+  }
+}
+
+  
+var btn = document.querySelector("#choixForm");
+btn.addEventListener("submit",choix_scenario);
+consol.log("aled");
